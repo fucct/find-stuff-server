ed3a1fd4cdb2bc65d4f765a9314846cdce1648a0
dd <dqrd123@gmail.com>
2020-11-03 14:07:25 +0900

    Initial commit

---
+++ src/main/java/com/fucct/findstuff/FindStuffApplication.java
@@ -0,0 +1,13 @@
+package com.fucct.findstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindStuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindStuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fucct/findstuff/FindStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fucct.findstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindStuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
